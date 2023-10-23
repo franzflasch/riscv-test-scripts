e584e556030ea32107d9177a752b9904a1a6ec63
Franz Flasch <franz.flasch@gmx.at>
2023-10-23 13:42:05 +0000

    fix apply_patches.sh

--- apply_patches.sh
+++ apply_patches.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-root_dir=$(PWD)
+root_dir=${PWD}
 
 # riscv-compliance
-cd cd ${root_dir}/riscv-compliance
+cd ${root_dir}/riscv-compliance
 patch -p1 < ../patches/001-prepare-riscv-compliance.patch
 
-cd cd ${root_dir}/riscv-isa-sim
+cd ${root_dir}/riscv-isa-sim
 patch -p1 < ../patches/002-prepare-riscv-isa-sim.patch
